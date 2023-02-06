d2fb2db678c41f9950acadee04168c169bec551c
tva050 <trym755@gmail.com>
2023-02-06 15:03:18 +0100

    not sure

--- Oblig 2/optical_depth.py
+++ Oblig 2/optical_depth.py
@@ -17,38 +17,29 @@ wavelength, absorption_cross_section_col_N2, absorption_cross_section_col_O2, ab
 # Data from file sigma_ionization.mat
 sigma_ionization = sc.io.loadmat('sigma_ionization.mat')
 
+""" a) """
 
-""" 
-a) Make functions that calculate the optical depth as a function of altitude and wavelength,
-for vertical incidence, and for variable zenith-angle of the incident light. 
+z0 = 0 # m
+SZA = 0 # solar zenit angles, degrees
 
-- phi: are the wavelength-dependent absorption cross sections of species j
-- n_j(0): are the heights profile of the concentration of species j
-- kji: is the zenith angle of the incident light
-- z: is the altitude 
-"""
+height = height * 1e3 # conversion to m
+#mass_density = mass_density * 1e-3  # conversion to kg/m^3
+number_density = (O + N2 + O2)*1e-6
 
-z0 = 75_000 # m
-SZA = 60 # solar zenit angles, degrees
-
-# heigth conversion to m
-height = height * 1e3
-# mass density conversion to kg/m^3
-#mass_density = mass_density * 1e-3
-number_density = (O + N2 + O2)*1e6
-
-integrand = number_density[height>=z0] * (1 - ( (R_earth + z0)/(R_earth + height[height>=z0]) ) ** 2 * (np.sin(SZA)) ** 2 ) ** (-0.5)
+# Expression for the optical depth when the solar zenith angle is smaller than 90 degrees
+integrand = mass_density[height>=z0] * (1 - ( (R_earth + z0)/(R_earth + height[height>=z0]) ) ** 2 * (np.sin(SZA)) ** 2 ) ** (-0.5)
 integral = sc.integrate.trapz(integrand, height[height>=z0])
 
-# Now you just multiply this integral with the absorption cross section at each wavelength and you'll get the optical depth at a range 
-# of wavelengths for a particular SZA and altitude.
 
 multiplied = integral * absorption_cross_section_col_N2
 
-# Wavelegth conversion to angstroms
-wavelength = wavelength * 1e10
+wavelength = wavelength * 1e10 # conversion to angstrom (Å)
 
-plt.plot(wavelength, multiplied)
+def optical_depth(z0, SZA):
+    
+    
+# Plot the absorption cross section of N2 with varying wavelength from "phot_abs.dat"
+plt.plot(wavelength, absorption_cross_section_col_N2)
 plt.title('Absorption cross section of N2 with varying wavelength')
 plt.ylabel(R"$N_2$ cross section [m$^2$]")
 plt.xlabel('Wavelength [Å]')
@@ -56,7 +47,11 @@ plt.xlabel('Wavelength [Å]')
 plt.yscale('log')
 plt.show()
 
-""" 
-b) 
-"""
+plt.plot(wavelength, multiplied)
+plt.ylabel(R"$\tau$")
+plt.yscale('log')
+plt.show()
 
+
+
+""" b) """
