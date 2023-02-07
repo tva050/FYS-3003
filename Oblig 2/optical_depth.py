6344d007a91bee365d29ebc18258e64c2f1f2628
tva050 <trym755@gmail.com>
2023-02-07 12:27:57 +0100

    Even closer

--- Oblig 2/optical_depth.py
+++ Oblig 2/optical_depth.py
@@ -1,10 +1,9 @@
 import numpy as np
 import scipy as sc
 import matplotlib.pyplot as plt
+from matplotlib import colors
 
 plt.style.use('ggplot')
-
-
 
 """ Import data from file """
 height, O, N2, O2, mass_density, temperature_neutral = np.loadtxt('MSIS.dat', skiprows=18, unpack=True)
@@ -18,29 +17,28 @@ z0 = 0 # [m]
 SZA = 0 # solar zenit angles
 
 height = height * 1e3 # -> [m]
-number_density = density = (O + N2 + O2)*1e-6 # -> [m^-3]
+number_density = (O + N2 + O2)*1e-6 # -> [m^-3]
+density = O*1e3 , N2*1e3 , O2*1e3 # -> [m^-3]
 
-def optical_depth(wavelength, height, absorption_cross_section):
+def optical_depth(wavelength, height, absorption_cross_section, density):
     tau = np.zeros((len(height),len(wavelength)), dtype=float)
-    for z0 in range(len(height)):
+    for i in range(len(height)):
         for j in range(len(wavelength)):
-            integrand = number_density[height>=z0] * (1 - ( (R_earth + z0)/(R_earth + height[height>=z0]) ) ** 2 * (np.sin(SZA)) ** 2 ) ** (-0.5)
+            z0 = height[i]
+            integrand = density[height>=z0] * (1 - ( (R_earth + z0)/(R_earth + height[height>=z0]) ) ** 2 * (np.sin(SZA)) ** 2 ) ** (-0.5)
             integral = sc.integrate.trapz(integrand, height[height>=z0])
-            tau[z0][j] = integral * absorption_cross_section[j]
+            tau[i][j] = integral * absorption_cross_section[j]
     return tau
 
+sums_up = optical_depth(wavelength, height, absorption_cross_section_col_N2, N2) + optical_depth(wavelength, height, absorption_cross_section_col_O2, O2) + optical_depth(wavelength, height, absorption_cross_section_col_O, O)
 
-sums_up = optical_depth(wavelength, height, absorption_cross_section_col_N2) + optical_depth(wavelength, height, absorption_cross_section_col_O2) + optical_depth(wavelength, height, absorption_cross_section_col_O)
-print(np.shape(sums_up))
 
-plt.pcolormesh(wavelength*1e9, height*1e-3, sums_up*1e-3)
+
+plt.pcolormesh(wavelength*1e9, height*1e-3, sums_up , norm=colors.LogNorm(), cmap='viridis')
 plt.xlabel('Wavelength [nm]')
 plt.ylabel('Height [km]')
 plt.title('Optical depth')
 plt.colorbar()
 plt.show()
- 
-
 
 """ b) """
-
