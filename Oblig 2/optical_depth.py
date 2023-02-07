004fce9084f9578a3a15883cf315cbc52d791631
tva050 <trym755@gmail.com>
2023-02-07 15:06:04 +0100

    tests and plots

--- Oblig 2/optical_depth.py
+++ Oblig 2/optical_depth.py
@@ -14,12 +14,11 @@ sigma_ionization = sc.io.loadmat('sigma_ionization.mat')
 R_earth = 6.371e6 # radius of the earth, [m]
 
 z0 = 0 # [m]
-SZA = 50 *(np.pi/180) # solar zenit angles
+SZA = 0 *(np.pi/180) # solar zenit angle, [rad]
 
 height = height * 1e3 # -> [m]
 density = O*1e6 , N2*1e6 , O2*1e6 # -> [m^-3]
 """ a) """
-
 def optical_depth(wavelength, height, absorption_cross_section, density):
     tau = np.zeros((len(height),len(wavelength)), dtype=float)
     for i in range(len(height)):
@@ -44,12 +43,31 @@ def tau_close_to_one(wavelength, height, sums_up):
             tau.append(height[index]*1e-3)
     return tau
 
-plt.pcolormesh(wavelength*1e9, height*1e-3, sums_up , norm=colors.LogNorm())
-plt.plot(wavelength*1e9, tau_close_to_one(wavelength, height, sums_up), color='black')
-plt.xlabel('Wavelength [nm]')
-plt.ylabel('Height [km]')
-plt.colorbar(label='Optical depth')
-plt.xlim(4.9,100)
-plt.show()
+def plot_optical_depth(wavelength, height, sums_up):
+    plt.pcolormesh(wavelength*1e9, height*1e-3, sums_up , norm=colors.LogNorm())
+    plt.plot(wavelength*1e9, tau_close_to_one(wavelength, height, sums_up), color='black', linewidth=1)
+    plt.xlabel('Wavelength [nm]')
+    plt.ylabel('Height [km]')
+    plt.colorbar(label='Optical depth')
+    plt.xlim(4.9,100)
+    plt.title("Optical depth, SZA = "+ str(round(SZA*(180/np.pi))) + R"$^\circ$")
+    plt.show()
+
+def plot_absorption_cross_section():
+    plt.plot(wavelength*1e9, absorption_cross_section_col_N2, label='N$_2$')
+    plt.plot(wavelength*1e9, absorption_cross_section_col_O2, label='O$_2$', color = "black")
+    plt.plot(wavelength*1e9, absorption_cross_section_col_O, label='O', color = "teal")
+    plt.xlabel('Wavelength [nm]')
+    plt.ylabel('Absorption cross section [m$^2$]')
+    plt.title("Absorption cross section")
+    plt.legend(loc='upper left', borderaxespad=0.5, fontsize=10, frameon=True, ncol=3, fancybox=True, shadow=True)
+    plt.show()
+    
+
+if __name__ == "__main__":
+    plot_optical_depth(wavelength, height, sums_up)
+    #plot_absorption_cross_section()
+    
+    
 
 """ b) """
