599c29a82ecda4e37fcd26c0a10c850d5159a30e
tva050 <trym755@gmail.com>
2023-02-07 11:20:22 +0100

    maby closer

--- Oblig 2/optical_depth.py
+++ Oblig 2/optical_depth.py
@@ -18,19 +18,10 @@ z0 = 0 # [m]
 SZA = 0 # solar zenit angles
 
 height = height * 1e3 # -> [m]
-number_density = (O + N2 + O2)*1e-6 # -> [m^-3]
-#wavelength = wavelength * 1e-1 # -> [nm]
-
-# Expression for the optical depth when the solar zenith angle is smaller than 90 degrees
-
-integrand = mass_density[height>=z0] * (1 - ( (R_earth + z0)/(R_earth + height[height>=z0]) ) ** 2 * (np.sin(SZA)) ** 2 ) ** (-0.5)
-integral = sc.integrate.trapz(integrand, height[height>=z0])
-
-
-multiplied = integral * absorption_cross_section_col_N2
+number_density = density = (O + N2 + O2)*1e-6 # -> [m^-3]
 
 def optical_depth(wavelength, height, absorption_cross_section):
-    tau = np.zeros((len(height), len(wavelength)), dtype=float)
+    tau = np.zeros((len(height),len(wavelength)), dtype=float)
     for z0 in range(len(height)):
         for j in range(len(wavelength)):
             integrand = number_density[height>=z0] * (1 - ( (R_earth + z0)/(R_earth + height[height>=z0]) ) ** 2 * (np.sin(SZA)) ** 2 ) ** (-0.5)
@@ -40,24 +31,15 @@ def optical_depth(wavelength, height, absorption_cross_section):
 
 
 sums_up = optical_depth(wavelength, height, absorption_cross_section_col_N2) + optical_depth(wavelength, height, absorption_cross_section_col_O2) + optical_depth(wavelength, height, absorption_cross_section_col_O)
+print(np.shape(sums_up))
 
-plt.pcolormesh(wavelength*1e9, height*1e-3, sums_up)
-plt.colorbar()
-plt.title('Optical depth')
-plt.ylabel('Height [km]')
+plt.pcolormesh(wavelength*1e9, height*1e-3, sums_up*1e-3)
 plt.xlabel('Wavelength [nm]')
+plt.ylabel('Height [km]')
+plt.title('Optical depth')
+plt.colorbar()
 plt.show()
  
- 
-""" # Plot the absorption cross section of N2 with varying wavelength from "phot_abs.dat"
-plt.plot(wavelength, multiplied*1e-3)
-plt.title('Absorption cross section of N2 with varying wavelength')
-plt.ylabel(R"$N_2$ optical depth [$km^{-1}$]")
-plt.xlabel('Wavelength [Å]')
-plt.xscale('log')
-plt.yscale('log')
-plt.show()
- """
 
 
 """ b) """
