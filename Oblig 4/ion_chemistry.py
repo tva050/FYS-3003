5f8f10d2c5f3e4099f060f0730b889635a28cd05
tva050 <trym755@gmail.com>
2023-05-04 14:37:34 +0200

    ok

--- Oblig 4/ion_chemistry.py
+++ Oblig 4/ion_chemistry.py
@@ -3,12 +3,15 @@ import matplotlib.pyplot as plt
 
 plt.style.use('ggplot')
 
-height, O, N2, O2, mass_density, temperature_nutral = np.loadtxt(r'Data\MSIS.dat', unpack = True ,skiprows=18, usecols= (0, 1, 2, 3, 4, 5))
+height, O, N2, O2, mass_density, nutral_temp = np.loadtxt(r'Data\MSIS.dat', unpack = True ,skiprows=118, usecols= (0, 1, 2, 3, 4, 5))
 wavelength, absorption_cross_section_N2, absorption_cross_section_O2, absorption_cross_section_O = np.loadtxt("Data\phot_abs.dat", skiprows=8, unpack=True)
 wavelength_fism, irradiance = np.loadtxt("Data\Fism_daily_hr19990216.dat",  unpack = True, skiprows = 51, max_rows=949, delimiter=',', usecols = (1, 2))
 electron_density, ion_temp, electron_temp = np.loadtxt("Data\IRI.dat", unpack = True, skiprows= 46, usecols=(1, 2, 3))
 
 
+# finds T_r by taking the mean of ion_temp and nutral_temp
+T_r = (ion_temp + nutral_temp)/2
+print(T_r)
 
 """ 
 Task 0: integrate the continuity-equations for 3600 seconds with a constant ionization-rate of
